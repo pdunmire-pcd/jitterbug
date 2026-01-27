a2b3ead78d117d3ff6d75ef20b00ba09cd77055c
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-01-27 19:47:51 +0000

    set up vector for 50 bugs

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,41 @@
-#include "bn_core.h"
-#include "bn_backdrop.h"
-#include "bn_color.h"
+#include <bn_core.h>
+#include <bn_backdrop.h>
+#include <bn_color.h>
+#include <bn_sprite_ptr.h>
+#include <bn_random.h>
+#include <bn_vector.h>
+#include <bn_keypad.h>
 
-int main() {
+#include "bn_sprite_items_ladybug.h"
+
+int main()
+{
     bn::core::init();
+
+    bn::random rng = bn::random();
 
     bn::backdrop::set_color(bn::color(30, 3, 5));
 
-    while(true) {
+    bn::vector<bn::sprite_ptr, 50> bugs = {};
+
+    // bn::sprite_ptr ladybug = bn::sprite_items::ladybug.create_sprite();
+
+    while (true)
+    {
+        for(bn::sprite_ptr& ladybug : bugs){
+            bn:: fixed new_x = ladybug.x() + rng.get_fixed(-1,1);
+            bn:: fixed new_y = ladybug.y() + rng.get_fixed(-1,1);
+
+            ladybug.set_x(new_x);
+            ladybug.set_y(new_y);
+        }
+        
+
+        if(bn::keypad::a_pressed()) {
+            bugs.push_back(bn::sprite_items::ladybug.create_sprite());
+        }
+
+        rng.update();
         bn::core::update();
     }
 }
